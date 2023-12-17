45537d7d33740be8e5a3f54fb3461bb28f2f6bb0
eddikan <imeekwere15@gmail.com>
2023-12-17 23:21:36 +0100

    feat: finish

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -11,31 +11,43 @@ const Experience = () => {
     >
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2010 - 2022</p>
+          <p className="text-sm text-designColor tracking-[4px]">2019 - 2023</p>
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            title="Web and mobile Engineer -remote"
+            subTitle="Eazipay - 03/03/2023 - present"
+            result="Nigeria"
+            des=" My work as a mobile and frontend developer in this company directly impacts how people manage their finances and investments. This can be personally rewarding, knowing that your work is making a real difference in users' lives."
           />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+            title="Frontend Software Developer - remote"
+            subTitle="Varroe - (2022 - 2023)"
+            result="Canada"
+            des="In my role, I was responsible for creating web pages and implementing data logic on the frontend of the web application. I utilized TypeScript, React.js, and Next.js to develop robust and scalable solutions"
           />
           <ResumeCard
-            title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+            title="Slate - hybrid"
+            subTitle="Slate - (2021 - 2022)"
+            result="Nigeria"
+            des=" UI/UX operations for a learning management system with secondary schools as the target audience,  the web application provided solutions from school management automation to comprehensive lesson delivery"
+          />
+          <ResumeCard
+            title="Full-Stack Software Developer - remote"
+            subTitle="SIT Technologies - (2022)"
+            result="United States"
+            des="I took ownership of building, deploying, and maintaining several of the company's projects utilizing Next.js, TypeScript, and Tailwind CSS."
+          />
+          <ResumeCard
+            title="Frontend Software Developer - hybrid"
+            subTitle="Ehealth4everyone - (2020 - 2021)"
+            result="Nigeria"
+            des="Built stable and maintainable data-driven applications and Worked closely with the design team to recommend solutions for a streamlined and user-friendly interface."
           />
         </div>
       </div>
-      <div>
+      {/* <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2001 - 2020</p>
           <h2 className="text-3xl md:text-4xl font-bold">Trainer Experience</h2>
@@ -60,7 +72,7 @@ const Experience = () => {
             des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
           />
         </div>
-      </div>
+      </div> */}
     </motion.div>
   );
 };
--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -1,10 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import Slider from "react-slick";
 import { RiStarFill } from "react-icons/ri";
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
-import Title from '../layouts/Title'
-import { testimonialOne,testimonialTwo, quote } from "../../assets";
-
+import Title from "../layouts/Title";
+import { testimonialOne, testimonialTwo, quote } from "../../assets";
 
 function SampleNextArrow(props) {
   const { onClick } = props;
@@ -31,62 +30,62 @@ function SamplePrevArrow(props) {
 }
 
 const Testimonial = () => {
-      const [dotActive, setDocActive] = useState(0);
-     const settings = {
-       dots: true,
-       infinite: true,
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       nextArrow:<SampleNextArrow />,
-       prevArrow:<SamplePrevArrow />,
-       beforeChange: (prev, next) => {
-         setDocActive(next);
-       },
-       appendDots: (dots) => (
-         <div
-           style={{
-             borderRadius: "10px",
-             padding: "10px",
-           }}
-         >
-           <ul
-             style={{
-               display: "flex",
-               gap: "15px",
-               justifyContent: "center",
-               marginTop: "20px",
-             }}
-           >
-             {" "}
-             {dots}{" "}
-           </ul>
-         </div>
-       ),
-       customPaging: (i) => (
-         <div
-           style={
-             i === dotActive
-               ? {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "#ff014f",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-               : {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "gray",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-           }
-         ></div>
-       ),
-     };
+  const [dotActive, setDocActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (prev, next) => {
+      setDocActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            gap: "15px",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          {" "}
+          {dots}{" "}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: "12px",
+                height: "12px",
+                color: "blue",
+                background: "#ff014f",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }
+            : {
+                width: "12px",
+                height: "12px",
+                color: "blue",
+                background: "gray",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }
+        }
+      ></div>
+    ),
+  };
   return (
     <section
       id="testimonial"
@@ -101,16 +100,16 @@ const Testimonial = () => {
           <div className="w-full">
             <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
               <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
+                {/* <img
                   className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
                   src={testimonialOne}
                   alt="testimonialOne"
-                />
-                <div className="w-full flex flex-col justify-end">
+                /> */}
+                <div className="w-full flex flex-col justify-end my-40">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
+                    Kelvin - Azibagirramani Mansi
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  {/* <h3 className="text-2xl font-bold">Jone Duone Joe</h3> */}
                   <p className="text-base tracking-wide text-gray-500">
                     Operation Officer
                   </p>
@@ -121,12 +120,12 @@ const Testimonial = () => {
                 <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      {/* <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
                         Travel Mobile App Design.
-                      </h3>
-                      <p className="text-base text-gray-400 mt-3">
+                      </h3> */}
+                      {/* <p className="text-base text-gray-400 mt-3">
                         via Upwork - Mar 4, 2015 - Aug 30, 2021 test
-                      </p>
+                      </p> */}
                     </div>
                     <div className="text-yellow-500 flex gap-1">
                       <RiStarFill />
@@ -137,11 +136,10 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                    dolorum, eos natus ipsum numquam veniam officia
-                    necessitatibus ratione quos debitis exercitationem
-                    repudiandae facilis id neque nihil accusantium perspiciatis
-                    repellat? Iste.
+                    Ime has demonstrated exceptional aptitude for rapid learning
+                    and adaptability under my supervision, starting from his
+                    internship until his promotion to a full-time staff position
+                    at EhealthforEveryone in Abuja
                   </p>
                 </div>
               </div>
@@ -151,17 +149,17 @@ const Testimonial = () => {
 
           <div className="w-full">
             <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
+              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r  from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
+                {/* <img
                   className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
                   src={testimonialTwo}
                   alt="testimonialTwo"
-                />
-                <div className="w-full flex flex-col justify-end">
+                /> */}
+                <div className="w-full flex flex-col my-40 justify-end">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
+                    Dennis - Shaba
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  {/* <h3 className="text-2xl font-bold">Jone Duoness Joe</h3> */}
                   <p className="text-base tracking-wide text-gray-500">
                     Operation Officer
                   </p>
@@ -172,12 +170,12 @@ const Testimonial = () => {
                 <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      {/* <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
                         Travel Mobile App Design.
-                      </h3>
-                      <p className="text-base text-gray-400 mt-3">
+                      </h3> */}
+                      {/* <p className="text-base text-gray-400 mt-3">
                         via Upwork - Mar 4, 2015 - Aug 30, 2021 test
-                      </p>
+                      </p> */}
                     </div>
                     <div className="text-yellow-500 flex gap-1">
                       <RiStarFill />
@@ -188,11 +186,15 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                    dolorum, eos natus ipsum numquam veniam officia
-                    necessitatibus ratione quos debitis exercitationem
-                    repudiandae facilis id neque nihil accusantium perspiciatis
-                    repellat? Iste.
+                    Throughout our extensive four-year collaboration on multiple
+                    projects, Ime consistently stood out as an exceptionally
+                    creative and proficient web developer. His remarkable
+                    ability to conceptualize unique designs, coupled with his
+                    adeptness in implementing highly effective strategies,
+                    positioned him as the foremost talent I have encountered in
+                    my professional career. Ime's invaluable contributions
+                    significantly contributed to the success of our endeavors,
+                    reflecting his unwavering commitment to excellence.
                   </p>
                 </div>
               </div>
@@ -203,18 +205,18 @@ const Testimonial = () => {
           <div className="w-full">
             <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
               <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
+                {/* <img
                   className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
                   src={testimonialOne}
                   alt="testimonialOne"
-                />
-                <div className="w-full flex flex-col justify-end">
+                /> */}
+                <div className="w-full flex flex-col my-40 justify-end">
                   <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
+                    Henry - Obadoni
                   </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  {/* <h3 className="text-2xl font-bold">Jone Duone Joe</h3> */}
                   <p className="text-base tracking-wide text-gray-500">
-                    Operation Officer
+                    Project Supervisor 
                   </p>
                 </div>
               </div>
@@ -223,12 +225,12 @@ const Testimonial = () => {
                 <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                     <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      {/* <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
                         Travel Mobile App Design.
                       </h3>
                       <p className="text-base text-gray-400 mt-3">
                         via Upwork - Mar 4, 2015 - Aug 30, 2021 test
-                      </p>
+                      </p> */}
                     </div>
                     <div className="text-yellow-500 flex gap-1">
                       <RiStarFill />
@@ -239,11 +241,14 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                    dolorum, eos natus ipsum numquam veniam officia
-                    necessitatibus ratione quos debitis exercitationem
-                    repudiandae facilis id neque nihil accusantium perspiciatis
-                    repellat? Iste.
+                    As Ime's supervisor at Dotmac Broadband Company in Abuja, I
+                    witnessed his significant contributions as a frontend
+                    developer. He excelled in software platform development, UI
+                    design, and web page design. Ime played a pivotal role in
+                    shaping and implementing web strategies, attending to both
+                    details and the bigger picture. His coding skills were
+                    excellent, and his unique and innovative designs were highly
+                    valued by the company.
                   </p>
                 </div>
               </div>
@@ -253,6 +258,6 @@ const Testimonial = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Testimonial
+export default Testimonial;
